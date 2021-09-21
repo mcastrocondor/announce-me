36cd979cdac6f1ddaebd7fdc7e6fe951d613555f
Melisa CR <mcastro@condorlabs.io>
2021-09-21 10:53:33 -0500

    Fix generation token

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ var moment = require("moment");
 var config = require("../config/token");
 
 exports.ensureAuthenticated = function (req, res, next) {
-    console.log(req.headers);
+    console.log('req.headers' , req.headers);
   if (!req.headers.authorization) {
     return res
       .status(403)
@@ -12,8 +12,8 @@ exports.ensureAuthenticated = function (req, res, next) {
 
   var token = req.headers.authorization;
   console.log('configgg token ', config.TOKEN_SECRET);
-  var payload = jwt.decode(token, config.TOKEN_SECRET, true);
-console.log('payload ',payload);
+  var payload = jwt.decode(token, config.TOKEN_SECRET);
+  console.log('payload ',payload);
   if (payload.exp <= moment().unix()) {
     return res.status(401).send({ message: "Token have expired" });
   }
