98c993a182387814a3f9959819dc7fab5bc0cae7
Melisa CR <mcastro@condorlabs.io>
2021-09-22 09:51:32 -0500

    Add configuration for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
       name: 'Announce-Me',
-      script: 'src/index', // the path of the script you want to execute,
+      script: 'node src/index', // the path of the script you want to execute,
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
       instances: 1,
       autorestart: true,
